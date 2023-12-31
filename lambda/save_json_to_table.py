df6a809c8dbcd37f7feffa4c77533ccefe7010b0
prateek <prateek.patel@bricklane.com>
2023-12-31 11:40:14 +0000

    Print event and partition data by timestamp
    The commit includes a print statement to log the event in the lambda_handler method. Also, it modifies the way data is stored in the S3 bucket: instead of attaching a timestamp to each row, the data is now partitioned by the 'timestamp' column.

--- lambda/save_json_to_table.py
+++ lambda/save_json_to_table.py
@@ -4,23 +4,24 @@ import pandas as pd
 
 
 def lambda_handler(event, context):
+    print(event)
     try:
         table = event["table"]
-        timestamp = datetime.now().strftime("%Y%m%dT%H%M%S")
-        df = pd.DataFrame.from_dict([event]).drop("table", axis="columns").assign(timestamp=timestamp)
-        filepath = f"s3://ds-dev-bkt/{table}/"
+        df = pd.DataFrame.from_dict([event]).drop("table", axis="columns")
+        filepath = f"s3://ds-dev-bkt/{table}"
         wr.s3.to_csv(df,
                      filepath,
                      index=False,
                      dataset=True,
                      mode="overwrite_partitions",
                      database="default",
+                     partition_cols=["timestamp"],
                      table=table,
                      schema_evolution=True
                      )
 
         return {
-            'body': filepath,
+            'body': f"{filepath}/{event['timestamp']}",
             'status': 200
         }
     except Exception as e:
